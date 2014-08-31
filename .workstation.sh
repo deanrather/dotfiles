657e4655af1e1652707a6035a38fc000783cd28d
Dean Rather <deanrather@gmail.com>
2014-09-01 02:29:20 +1000

    updated with workstation push

--- .workstation.sh
+++ .workstation.sh
@@ -524,8 +524,8 @@
     complete -W "$workstation_function_names" workstation
     
     # Ensure SSH agent is running
-    eval $(ssh-agent) > /dev/null 2>&1 &
-    ssh-add ~/.ssh/*
+    # eval $(ssh-agent) > /dev/null 2>&1 &
+    # ssh-add ~/.ssh/*
     
 ## TODO ##
 
