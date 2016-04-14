af00664aad3f1711545d316547601d68397f7f0c
Dean Rather <deanrather@gmail.com>
2016-04-14 15:44:17 +1000

    refactor tmux

--- aliases.sh
+++ aliases.sh
@@ -9,6 +9,7 @@ alias wp="workstation push && workstation pull"
 alias v="vagrant"
 alias vup="vagrant up"
 alias vssh="vagrant ssh"
+alias vs="vagrant status"
 
 # Git
 alias g="git"
@@ -19,11 +20,13 @@ alias gf="git fetch && git fetch --tags"
 alias gb="git_branch"
 alias ga="git add ."
 alias gc="git commit -am"
+alias ta="tig --all"
+alias gsu="git submodule update --init --recursive"
 
 # Edit
 alias edit="vim"
 alias e="edit"
-alias ea="edit ~/.workstation.d/aliases.sh"
+alias ea="edit ~/.workstation.d/aliases.sh && wr"
 alias eg="edit ~/.gitrc"
 alias et="edit ~/.tmux.conf"
 alias ev="edit ~/.vimrc"
