4662fe00836e50e0b03c901b942c968e570fc1ab
Dean Rather <deanrather@gmail.com>
2017-07-16 19:52:47 +1000

    importat comment

--- functions.sh
+++ functions.sh
@@ -758,6 +758,7 @@ grant_user_superpowers()
 {
   echo "funtion temporarily disabled for breaking 2 computers now. todo: check args";
   exit 1
+  # pkexec to the rescue...
     USERNAME="$1"
     echo "$USERNAME ALL=(ALL) NOPASSWD: ALL" | sudo tee "/etc/sudoers.d/$USERNAME-sudo-nopasswd"
 	sudo chmod 0440 "/etc/sudoers.d/$USERNAME-sudo-nopasswd"
